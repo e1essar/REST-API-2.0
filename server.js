2a2cb8e212057e3c3f5d47538da11b57b7ca98d2
e1essar <igorvaliulin770@gmail.com>
2022-10-15 03:05:17 +0700

    init reqs and db

--- server.js
+++ server.js
@@ -1,11 +1,56 @@
-const express = require('express')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const PORT = process.env.PORT || 8080
+const app = express();
 
-const app = express()
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-app.get('/', (req, res) => {
-    res.send('server started')
-})
+app.use(cors(corsOptions));
 
-app.listen(PORT, () => console.log('server started on ' + PORT))
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const db = require("./models");
+const Role = db.role;
+
+function initial() {
+  Role.create({
+    id: 1,
+    name: "user"
+  });
+ 
+  Role.create({
+    id: 2,
+    name: "moderator"
+  });
+ 
+  Role.create({
+    id: 3,
+    name: "admin"
+  });
+}
+
+db.sequelize.sync({force: true}).then(() => {
+  console.log('Drop and Resync Db');
+  initial();
+});
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Hello World" });
+});
+
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
