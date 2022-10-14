d3798dfbc177e4cfb09c7313166fbfdbf3bc663e
e1essar <igorvaliulin770@gmail.com>
2022-10-14 09:42:07 +0700

    init sever

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 8080
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('OK')
+    res.send('server started')
 })
 
 app.listen(PORT, () => console.log('server started on ' + PORT))
